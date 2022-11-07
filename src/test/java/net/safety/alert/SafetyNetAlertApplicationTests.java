ca0b7191e5310e55b8b034b7208549a8e0c7fc20
trimok <trimok@mail.fr>
2022-11-07 16:16:10 +0100

    Initial commit

---
+++ src/test/java/net/safety/alert/SafetyNetAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package net.safety.alert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
