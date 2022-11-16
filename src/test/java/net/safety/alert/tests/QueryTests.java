77680718f2cd076bac9e8044196d8af0486eb054
trimok <trimok@mail.fr>
2022-11-16 12:12:21 +0100

    Implementation TEST endpoint GET /flood/stations?stations=

--- src/test/java/net/safety/alert/tests/QueryTests.java
+++ src/test/java/net/safety/alert/tests/QueryTests.java
@@ -25,8 +25,10 @@ import net.safety.alert.dto.ChildrenByAddressDTO;
 import net.safety.alert.dto.ChildrensByAddressDTO;
 import net.safety.alert.dto.PersonByAddressDTO;
 import net.safety.alert.dto.PersonByStationDTO;
+import net.safety.alert.dto.PersonGroupByAddressByListStationDTO;
 import net.safety.alert.dto.PersonsByAddressDTO;
 import net.safety.alert.dto.PersonsByStationDTO;
+import net.safety.alert.dto.PersonsGroupByAddressByListStationDTO;
 import net.safety.alert.dto.PhonesByStationDTO;
 
 @SpringBootTest
@@ -41,9 +43,13 @@ class QueryTests {
 	private static final int FIRESTATION_3_CHILDREN_COUNT = 3;
 	private static final int FIRESTATION_3_ADULT_COUNT = 8;
 	private static final int FIRESTATION_3_PERSONS_COUNT = 11;
+	private static final int STATION_LIST_3_4_ADDRESS_COUNT = 5;
+
 	// Constants
 	private final static String FIRESTATION_3 = "3";
 	private final static String FIRESTATION_4 = "4";
+	private static final String STATION_LIST_3_4 = "3,4";
+
 	private final static String ADDRESS_CULVER = "1509 Culver St";
 	private static final int ADDRESS_CULVER_PERSONS_COUNT = 5;
 
@@ -60,7 +66,6 @@ class QueryTests {
 
 			tDTO = mapper.readValue(result.getResponse().getContentAsString(), type);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
@@ -69,7 +74,7 @@ class QueryTests {
 
 	/********************** TEST GET /firestation?stationNumber= QueryController.findPersonsByStationDTO **********/
 
-	public static Stream<Arguments> whenStationIsChoosen_ShouldReturnPersonsAdultsChildrenProvider() {
+	public static Stream<Arguments> whenStationIsGiven_ShouldReturnPersonsAdultsChildrenProvider() {
 		// GIVEN
 		return Stream.of(
 				Arguments.arguments(FIRESTATION_3, FIRESTATION_3_PERSONS_COUNT, FIRESTATION_3_ADULT_COUNT,
@@ -80,9 +85,9 @@ class QueryTests {
 
 	@DisplayName("GET /firestation?stationNumber= : ")
 	@ParameterizedTest(name = "when Station is {0}, should return {1} persons, {2} adults, and  {3} children")
-	@MethodSource("whenStationIsChoosen_ShouldReturnPersonsAdultsChildrenProvider")
-	public void whenStationIsChoosen_ShouldReturnPersonsAdultsChildren(String station, long personsCount,
-			long adultCount, long childrenCount) throws Exception {
+	@MethodSource("whenStationIsGiven_ShouldReturnPersonsAdultsChildrenProvider")
+	public void whenStationIsGiven_ShouldReturnPersonsAdultsChildren(String station, long personsCount, long adultCount,
+			long childrenCount) throws Exception {
 
 		// WHEN
 		PersonsByStationDTO personsByStationDTO = getDtoFromUrlGet("/firestation?stationNumber=" + station,
@@ -102,15 +107,15 @@ class QueryTests {
 
 	/********************** TEST GET /childAlert?address= QueryController.findChildrensByAddressDTO **********/
 
-	public static Stream<Arguments> whenAddressIsChoosen_ShouldReturnChildrenProvider() {
+	public static Stream<Arguments> whenAddressIsGiven_ShouldReturnChildrenProvider() {
 		// GIVEN
 		return Stream.of(Arguments.arguments(ADDRESS_CULVER, ADDRESS_CULVER_ADULT_COUNT, ADDRESS_CULVER_CHILD_COUNT));
 	}
 
 	@DisplayName("GET /childAlert?address= : ")
 	@ParameterizedTest(name = "when Address is {0},  should return {1} adults, and  {2} children")
-	@MethodSource("whenAddressIsChoosen_ShouldReturnChildrenProvider")
-	public void whenAddressIsChoosen_ShouldReturnChildren(String address, long adultCount, long childrenCount)
+	@MethodSource("whenAddressIsGiven_ShouldReturnChildrenProvider")
+	public void whenAddressIsGiven_ShouldReturnChildren(String address, long adultCount, long childrenCount)
 			throws Exception {
 
 		// WHEN
@@ -135,15 +140,15 @@ class QueryTests {
 
 	/********************** TEST GET /phoneAlert?firestation= QueryController.findPhonesByStationDTO **********/
 
-	public static Stream<Arguments> whenFireStationIsChoosen_ShouldReturnPhoneListProvider() {
+	public static Stream<Arguments> whenFireStationIsGiven_ShouldReturnPhoneListProvider() {
 		// GIVEN
 		return Stream.of(Arguments.arguments(FIRESTATION_3, FIRESTATION_3_PHONE_COUNT));
 	}
 
 	@DisplayName("GET /phoneAlert?firestation= : ")
 	@ParameterizedTest(name = "when firestation is {0},  should return {1} phones")
-	@MethodSource("whenFireStationIsChoosen_ShouldReturnPhoneListProvider")
-	public void whenFireStationIsChoosen_ShouldReturnPhoneList(String fireStation, long phonesCount) throws Exception {
+	@MethodSource("whenFireStationIsGiven_ShouldReturnPhoneListProvider")
+	public void whenFireStationIsGiven_ShouldReturnPhoneList(String fireStation, long phonesCount) throws Exception {
 
 		// WHEN
 		PhonesByStationDTO phonesDTO = getDtoFromUrlGet("/phoneAlert?firestation=" + fireStation,
@@ -157,15 +162,15 @@ class QueryTests {
 
 	/********************** TEST GET /fire?address= QueryController.findPersonsByAddressDTO **********/
 
-	public static Stream<Arguments> whenAddressIsChoosen_ShouldReturnPersonListProvider() {
+	public static Stream<Arguments> whenAddressIsGiven_ShouldReturnPersonListProvider() {
 		// GIVEN
 		return Stream.of(Arguments.arguments(ADDRESS_CULVER, ADDRESS_CULVER_PERSONS_COUNT));
 	}
 
 	@DisplayName("GET /fire?address= : ")
 	@ParameterizedTest(name = "when address is {0},  should return {1} persons with medical records")
-	@MethodSource("whenAddressIsChoosen_ShouldReturnPersonListProvider")
-	public void whenAddressIsChoosen_ShouldReturnPersonList(String address, long personsCount) throws Exception {
+	@MethodSource("whenAddressIsGiven_ShouldReturnPersonListProvider")
+	public void whenAddressIsGiven_ShouldReturnPersonList(String address, long personsCount) throws Exception {
 
 		// WHEN
 		PersonsByAddressDTO personsDTO = getDtoFromUrlGet("/fire?address=" + address,
@@ -179,6 +184,43 @@ class QueryTests {
 		List<String> allergies = person.getAllergies();
 
 		assertThat(persons.size()).isEqualTo(personsCount);
+		assert (!person.getLastName().isEmpty());
+		assert (!person.getPhone().isEmpty());
+		assert (!person.getFireStation().isEmpty());
+		assertThat(person.getAge()).isGreaterThan(0);
+		assertThat(medications.size()).isGreaterThan(0);
+		assertThat(allergies.size()).isGreaterThan(0);
+	}
+
+	/********* TEST GET /flood/stations?stations= QueryController.findPersonsGroupByAddressByListStationDTO *********/
+
+	public static Stream<Arguments> whenListStationIsGiven_ShouldReturnPersonListGroupByAddressProvider() {
+		// GIVEN
+		return Stream.of(Arguments.arguments(STATION_LIST_3_4, STATION_LIST_3_4_ADDRESS_COUNT, ADDRESS_CULVER,
+				ADDRESS_CULVER_PERSONS_COUNT));
+	}
+
+	@DisplayName("GET /flood/stations?stations= : ")
+	@ParameterizedTest(name = "when listeStation is {0},  should return {1} address groups with {2} persons for the group at address {3}")
+	@MethodSource("whenListStationIsGiven_ShouldReturnPersonListGroupByAddressProvider")
+	public void whenListStationIsGiven_ShouldReturnPersonListGroupByAddress(String addressList, long groupCount,
+			String groupName, long personsGroupCount) throws Exception {
+
+		// WHEN
+		PersonsGroupByAddressByListStationDTO mapPersonsDTO = getDtoFromUrlGet(
+				"/flood/stations?stations=" + addressList, new TypeReference<PersonsGroupByAddressByListStationDTO>() {
+				});
+
+		// THEN
+		Map<String, List<PersonGroupByAddressByListStationDTO>> mapPersons = mapPersonsDTO.getPersonsMap();
+		List<PersonGroupByAddressByListStationDTO> personsGroup = mapPersons.get(groupName);
+		PersonGroupByAddressByListStationDTO person = personsGroup.get(1);
+
+		assertThat(mapPersons.size()).isEqualTo(groupCount);
+		assertThat(mapPersons.size()).isEqualTo(personsGroupCount);
+
+		Map<String, String> medications = person.getMedications();
+		List<String> allergies = person.getAllergies();
 		assert (!person.getLastName().isEmpty());
 		assert (!person.getPhone().isEmpty());
 		assert (!person.getFireStation().isEmpty());
